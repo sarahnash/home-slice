8764fd620a558bf983b35ab0a8fc1dbdd76a4c74
Elizabeth Creech <elizabeth@timbergrove.com>
2018-11-17 10:48:39 -0600

    saving progress on profile db script

---
+++ scripts/ec_prfile_db.js
@@ -0,0 +1,22 @@
+// This is the test scipt for saving User profile data to Firebase
+
+// Reference to the database service
+var database = firebase.database();
+
+function writeUserProfileData(userID, amenities, favorites) {
+    var profileDateTimeStamp = Date.now
+    firebase.database().ref('users/' + userID).set({
+        lastUpdated: profileDateTimeStamp
+        amenitySettings: amenities
+        favoriteSettings: favorites
+    })
+}
+
+function writeUserProfileData(userID, amenities, favorites) {
+    var profileDateTimeStamp = Date.now
+    firebase.database().ref('users/' + userID).set({
+        lastUpdated: profileDateTimeStamp
+        amenitySettings: amenities
+        favoriteSettings: favorites
+    })
+}
