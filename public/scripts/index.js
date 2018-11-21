760f81da3edad50f6bb113debbf5a7cfd831736b
sarahnash <sarahnash1112@gmail.com>
2018-11-20 21:04:31 -0600

    moving about us to public folder

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,95 +1,87 @@
+const $ = window.jQuery
+
 const apiKey = `2tFurqRYbB_R4WkxVxs_74cZPMtIPu_9c62p69PmjCW6JEtH6_pm0XrSEqQqjYsP7aMQRE8RG9sYlcjbjcLjpUmea4hqSXaItF08axHXVF358SAxKUXTkDFLhm3wW3Yx`
 const clientID = `6PaudvUaHDgnvmwq8HFv5w`;
 (function () {
-    document.addEventListener('DOMContentLoaded', init)
+  document.addEventListener('DOMContentLoaded', init)
 
-    function corseAPI() {
-        $.ajaxPrefilter(function (options) {
-            if (options.crossDomain && jQuery.support.cors) {
-                options.url = 'https://cors-anywhere.herokuapp.com/' + options.url
-            }
-        })
-    }
+  function corseAPI () {
+    $.ajaxPrefilter(function (options) {
+      if (options.crossDomain && jQuery.support.cors) {
+        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url
+      }
+    })
+  }
 
+  function init () {
+    corseAPI()
+    console.info('The DOM has loaded')
+    document.getElementById('search-form').addEventListener('submit', getInfo)
+    getData('NYC')
+    $container = $('#render-places')
+  }
 
+  // Obtain the information for search
+  function getInfo (evt) {
+    evt.preventDefault()
+    console.info('Typing')
+    let info = takeInput(document.getElementById('search-bar').value.toLowerCase())
+    console.info('This is the info ' + info)
+    getData(info)
+  }
 
-    function init() {
-        corseAPI()
-        console.info('The DOM has loaded')
-        document.getElementById('search-form').addEventListener('submit', getInfo)
-        getData('NYC')
-        $container = $('#render-places')
-    }
+  getData('nyc').then
 
+  function getData (location) {
+    // corseAPI()
+    $.ajax({
+      url: `https://api.yelp.com/v3/businesses/search?location=${location}&limit=10`,
+      headers: {
+        'Authorization': `Bearer ${apiKey}`
+      }
+    })
+      .then(function (res) {
+        return res.businesses
+      })
+      .then(giveBuisInfo)
+      .then(function (res) {
+        console.info(res)
+      })
+      .catch(function (error) {
+        console.alert(error)
+      })
+  }
+  function displayData (array) {
+    // To do something
+  }
+  function giveBuisInfo (array) {
+    let newArray = array.map(getBuisData)
+    return Promise.all(newArray)
+  }
+  function getBuisData (array) {
+    id = array.id
+    return $.ajax({
+      url: `https://api.yelp.com/v3/businesses/${id}`,
+      headers: {
+        'Authorization': `Bearer ${apiKey}`
+      }
+    })
+      .then(function (res) {
+        return res
+      })
+      .catch(function (error) {
+        console.error(error)
+      })
+  }
+  function displayData (obj) {
+    console.info('This is the display Data', obj)
+  }
 
-
-
-    //Obtain the information for search
-    function getInfo(evt) {
-        evt.preventDefault()
-        console.info('Typing')
-        let info = takeInput(document.getElementById('search-bar').value.toLowerCase())
-        console.info('This is the info ' + info)
-        getData(info)
-    }
-
-getData('nyc').then
-
-
-    function getData(location) {
-        // corseAPI()
-        $.ajax({
-                url: `https://api.yelp.com/v3/businesses/search?location=${location}&limit=10`,
-                headers: {
-                    'Authorization': `Bearer ${apiKey}`
-                }
-            })
-            .then(function (res) {
-                return res.businesses
-            })
-            .then(giveBuisInfo)
-            .then(function (res){
-                console.info(res)
-            })
-            .catch(function (error) {
-                console.alert(error)
-            })
-
-    }
-    function displayData (array){
-        // To do something
-    }
-    function giveBuisInfo(array){
-       let newArray =  array.map(getBuisData)
-        return Promise.all(newArray)
-    }
-    function getBuisData(array) {
-        id = array.id
-       return $.ajax({
-                url: `https://api.yelp.com/v3/businesses/${id}`,
-                headers: {
-                    'Authorization': `Bearer ${apiKey}`
-                }
-            })
-            .then(function (res){
-                return res
-            })
-            .catch(function (error){
-                console.error(error)
-            })
-    }
-    function displayData (obj){
-        console.info('This is the display Data', obj)
-    }
-
-    function takeInput(search) {
-        let urlEncodedSearchString = encodeURIComponent(search)
-        return urlEncodedSearchString
-    }
-
-
+  function takeInput (search) {
+    let urlEncodedSearchString = encodeURIComponent(search)
+    return urlEncodedSearchString
+  }
 })()
-
 
 // // TO ADD
 // <!-- Button trigger modal -->
