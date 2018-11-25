d15f0107ef6b6a23b9ea822bab54703e833dec84
KimjManansala <kimjmanansala@gmail.com>
2018-11-25 13:16:08 -0600

    Updated searching

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -20,29 +20,33 @@ const clientID = `6PaudvUaHDgnvmwq8HFv5w`;
 
     function init() {
         corseAPI()
-        const placesToEat = document.getElementById('places-to-eat')
+        const places = document.getElementById('places')
         console.info('The DOM has loaded')
         geoFindMe()
         document.getElementById('search-form').addEventListener('submit', getUserInfo)
-        placesToEat.addEventListener('click', displayModal)
-
-
+        places.addEventListener('click', placeClicked)
 
         //--------------------
         // Functions to get information from API and render the results from it
         //Obtain the information for search
+
         function getUserInfo(evt) {
             evt.preventDefault()
-            window.location.hash = "places-to-eat";
-            placesToEat.innerHTML = `<h1 class='loading'>Loading your yummy results<h1>`
+            window.location.hash = "places";
+            places.innerHTML = `<div id='waiting-for-food'>
+            <div class="loader"></div>
+            <p id='loading-text'>Loading your yummy results</p>
+          </div>
+      `
             let term = encodeURI(document.getElementById('search-bar').value.toLowerCase())
+            console.info(term)
             console.info('This is userSearchData', userSearchData)
             getDetailedBuis(term)
         }
 
         function getDetailedBuis(term) {
             $.ajax({
-                    url: `https://api.yelp.com/v3/businesses/search?latitude=${userSearchData.lat}&longitude=${userSearchData.long}&term=${term}&limit=25`,
+                    url: `https://api.yelp.com/v3/businesses/search?latitude=${userSearchData.lat}&longitude=${userSearchData.long}&term=${term}&limit=50`,
                     headers: {
                         'Authorization': `Bearer ${apiKey}`
                     }
@@ -50,7 +54,6 @@ const clientID = `6PaudvUaHDgnvmwq8HFv5w`;
                 .then(function (res) {
                     return res.businesses
                 })
-                .then(makesArrayOfBuisData)
                 .then(displayData)
                 .catch(function (error) {
 
@@ -58,16 +61,17 @@ const clientID = `6PaudvUaHDgnvmwq8HFv5w`;
 
         }
 
-        function renderCards(res) {
+        function createHTML(res) {
             if (res) {
                 return `
-            <div class="card bg-dark text-white" id='${res.id}'>
-                    <img class="card-img" src="${res.image_url}" alt="Card image ">
-                    <div class="card-img-overlay">
-                        <h5 class="card-title">${res.name}</h5>
-                        <p class="card-text">${res.price}</p>
-                        <p class="card-text">Hi</p>
-                </div>
+            <div class="card bg-dark text-white">
+                    <img class="card-img" src="${res.image_url}" alt="Card image">
+                        <div class="card-img-overlay">
+                            <h5 class="card-title">${res.name}</h5>
+                            <p class="card-text">${res.price}</p>
+                            <button type="button" class="btn btn-primary" id="${res.id}">Press for More info</button>
+                        </div>
+                    </div>
             </div>
             `
 
@@ -75,39 +79,40 @@ const clientID = `6PaudvUaHDgnvmwq8HFv5w`;
 
         }
 
-        function makesArrayOfBuisData(array) {
-            let newArray = array.map(getBuisData)
-            return Promise.all(newArray)
-        }
 
         function displayData(array) {
             console.info('this is the displayData', array)
-            let newArray = array.map(renderCards)
+            let newArray = array.map(createHTML)
             let displayArrray = newArray.join(' ')
-            placesToEat.innerHTML = displayArrray
+            places.innerHTML = displayArrray
         }
-
-        function getBuisData(array) {
-            id = array.id
-            return $.ajax({
+        function placeClicked(evt) {
+            console.info(evt.target.id)
+            let id = evt.target.id
+            $.ajax({
                     url: `https://api.yelp.com/v3/businesses/${id}`,
                     headers: {
                         'Authorization': `Bearer ${apiKey}`
                     }
                 })
-                .then(function (res) {
-                    return res
-                })
-                .catch(function (error) {
-
-                })
+                .then(displayModal)
         }
-        //-----------------------
 
-        function displayModal(evt) {
-            console.info(evt.target)
-            geoFindMe()
+        function displayModal(buis) {
+            console.info(buis)
+            return `
+            <div id="myModal" class="modal">
+
+                <div class="modal-content">
+                    <span class="close">&times;</span>
+                    <p>Some text in the Modal..</p>
+                </div>
+
+            </div>
+
+            `
         }
+
 
         function encodeURI(search) {
             let urlEncodedSearchString = encodeURIComponent(search)
@@ -115,7 +120,7 @@ const clientID = `6PaudvUaHDgnvmwq8HFv5w`;
         }
 
         function geoFindMe() {
-            let output = document.getElementById("places-to-eat");
+            let output = document.getElementById("places");
             console.info('GeoFindMe function has init')
 
             if (!navigator.geolocation) {
@@ -124,18 +129,21 @@ const clientID = `6PaudvUaHDgnvmwq8HFv5w`;
             }
 
             function success(position) {
+                document.getElementById('search-form').style.display = 'block'
+                document.getElementById('waiting-for-location').style.display = 'none'
                 let latitude = position.coords.latitude
                 let longitude = position.coords.longitude
                 userSearchData.lat = latitude
                 userSearchData.long = longitude
+                console.info(userSearchData)
 
 
             }
 
             function error() {
-                alert('Unable to locate position')
+                alert('Unable to locate position please reload the page')
             }
-
+            document.getElementById('search-form').style.display = 'none'
             navigator.geolocation.getCurrentPosition(success, error)
         }
 
