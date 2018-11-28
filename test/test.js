cd660c99e7efdfbf52713aa6e032339d6a49cba1
sarahnash <sarahnash1112@gmail.com>
2018-11-27 20:31:55 -0600

    haha

--- test/test.js
+++ test/test.js
@@ -1,26 +1,14 @@
 //Create the HTML card with the DATA for each
 function createHTML(res) {
   if (res) {
-      return `<div class="card bg-dark text-white">
-            <img class="card-img" src="${res.image_url}" alt="Card image">
-            <div class="card-img-overlay">
-                <h5 class="card-title">${res.name}</h5>
-                <p class="card-text">${res.price}</p>
-            </div>
-        </div>`}
+    return `<div class="card bg-dark text-white"><img class="card-img" src="${res.image_url}" alt="Card image"><div class="card-img-overlay"><h5 class="card-title">${res.name}</h5><p class="card-text">${res.price}</p></div></div>`}
 }
 
 var assert = require('assert')
 describe('Rendering', function() {
   it('should dynamically generate HTML', function () {
-    var dataObject = {image_url: 'www.testurl.com', name: 'KimJM', price: 100}
-    assert.equal( createHTML(dataObject),`<div class="card bg-dark text-white">
-    <img class="card-img" src="www.testurl.com" alt="Card image">
-    <div class="card-img-overlay">
-        <h5 class="card-title">KimJM</h5>
-        <p class="card-text">100</p>
-    </div>
-</div>`)
+    var dataObject = {image_url: 'www.banana.com', name: 'oakman', price: 100}
+    assert.equal( createHTML(dataObject),`<div class="card bg-dark text-white"><img class="card-img" src="www.banana.com" alt="Card image"><div class="card-img-overlay"><h5 class="card-title">oakman</h5><p class="card-text">100</p></div></div>`)
   })
 })
 
