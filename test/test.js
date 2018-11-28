5b4cc6b4225b4296631fed138b5ef9fccae37855
sarahnash <sarahnash1112@gmail.com>
2018-11-27 20:20:58 -0600

    mocha test

--- test/test.js
+++ test/test.js
@@ -1,14 +1,30 @@
+//Create the HTML card with the DATA for each
+function createHTML(res) {
+  if (res) {
+      return `
+        <div class="card bg-dark text-white">
+            <img class="card-img" src="${res.image_url}" alt="Card image">
+            <div class="card-img-overlay">
+                <h5 class="card-title">${res.name}</h5>
+                <p class="card-text">${res.price}</p>
+            </div>
+        </div>
+  `
+  }
+}
+
 var assert = require('assert')
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1)
-    })
+describe('Rendering', function() {
+  it('should dynamically generate HTML', function () {
+    var dataObject = {image_url: 'www.testurl.com', name: 'KimJM', price: 100}
+    assert.equal( createHTML(dataObject),`
+    <div class="card bg-dark text-white">
+        <img class="card-img" src="www.testurl.com" alt="Card image">
+        <div class="card-img-overlay">
+            <h5 class="card-title">KimJM</h5>
+            <p class="card-text">100</p>
+        </div>
+    </div>
+`)
   })
 })
-
-var assert = require('chai').assert;
-var displayArray = []
-
-assert.isArray(displayArray, 'is array')
-// comment
